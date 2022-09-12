a2190465c6021d5dcf97442528f6cddd12691a84
Infotoh <infotouch999@gmail.com>
2022-09-12 18:09:31 +0200

    first commit

---
+++ database/seeders/ConsultationSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConsultationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Consultation::create([
+            'number' => 2222,
+            'name' => 'name Consultation',
+            'consultion' => '22consultion consultion consultion consultion',
+        ]);
+    }
+}
