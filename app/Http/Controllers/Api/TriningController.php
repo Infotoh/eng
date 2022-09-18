8edb088b90b16a97d8cd3560ebec634197a241ac
Infotoh <infotouch999@gmail.com>
2022-09-18 20:41:40 +0200

    lllll

--- app/Http/Controllers/Api/TriningController.php
+++ app/Http/Controllers/Api/TriningController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Training;
 class TriningController extends Controller
 {
     //
