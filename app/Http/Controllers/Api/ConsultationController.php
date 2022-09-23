08535ef71fbaec47f7541f2b18c47152dda5dc31
Infotoh <infotouch999@gmail.com>
2022-09-23 16:59:24 +0200

    lllll

--- app/Http/Controllers/Api/ConsultationController.php
+++ app/Http/Controllers/Api/ConsultationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Consultation;
+use Illuminate\Support\Facades\Validator;
 
 class ConsultationController extends Controller
 {
